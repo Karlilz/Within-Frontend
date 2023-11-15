f3febb114e1a04895621be30f5940d941802de25
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-15 17:09:11 -0500

    WelcomePage background Image added

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,23 +1,64 @@
+// NO CSS
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const WelcomePage = () => {
+//   return (
+//     <div className="welcome-container">
+//       <header>
+//         <h1>Within</h1>
+//         <h2>Ignite the Possible</h2>
+//       </header>
+//       <div className="button-container">
+//         <Link to="/login">
+//           <button>Login</button>
+//         </Link>
+//         <Link to="/signup">
+//           <button>Signup</button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WelcomePage;
+
+
+// CSS
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const WelcomePage = () => {
-  return (
-    <div className="welcome-container">
-      <header>
-        <h1>Within</h1>
-        <h2>Ignite the Possible</h2>
-      </header>
-      <div className="button-container">
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/signup">
-          <button>Signup</button>
-        </Link>
+    const backgroundStyle = {
+      backgroundImage: 'url(/Images/WithinWelcome2.png)', 
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  
+    return (
+      <div style={backgroundStyle}>
+        <header>
+          <h1>Within</h1>
+          <h2>Ignite the Possible</h2>
+        </header>
+        <div className="button-container">
+          <Link to="/login" className="welcome-button">
+            Login
+          </Link>
+          <Link to="/signup" className="welcome-button">
+            Signup
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default WelcomePage;
+
+  
+
--- src/router.js
+++ src/router.js
@@ -6,12 +6,14 @@ import {createBrowserRouter,createRoutesFromElements,Route} from "react-router-d
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import App from './App';
+import Welcome from './pages/Welcome';
 
 // Main Router component
 const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<App />}>
+            <Route path='/Within' element={<Welcome/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Signup/>}/>
         </Route>
