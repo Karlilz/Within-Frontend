cdf8588a671afcf10d99aef90d36417e90d45431
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-16 02:19:24 -0500

    added home button to goal page

--- src/pages/Goal.js
+++ src/pages/Goal.js
@@ -278,25 +278,231 @@
 
 
 
+// import React, { useState, useEffect } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// const URL = "http://localhost:3000/";
+
+// const Goal = () => {
+//   const [newGoal, setNewGoal] = useState('');
+//   const [newDueDate, setNewDueDate] = useState('');
+//   const [goals, setGoals] = useState([]);
+//   const [editingGoalId, setEditingGoalId] = useState(null);
+//   const [editedGoalText, setEditedGoalText] = useState('');
+//   const [editedDueDate, setEditedDueDate] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleInputChange = (e) => {
+//     setNewGoal(e.target.value);
+//   };
+
+//   const handleDueDateChange = (e) => {
+//     setNewDueDate(e.target.value);
+//   };
+
+//   const handleAddGoal = async () => {
+//     if (newGoal.trim() !== '') {
+//       try {
+//         // Make a POST request to create a new goal
+//         const response = await fetch(URL + 'goals', {
+//           method: 'POST',
+//           headers: {
+//             'Authorization': `Token ${localStorage.getItem('token')}`,
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({
+//             content: newGoal,
+//             dueDate: newDueDate,
+//           }),
+//         });
+
+//         if (!response.ok) {
+//           throw new Error('Failed to add goal');
+//         }
+
+//         const newGoalItem = await response.json();
+//         setGoals([...goals, newGoalItem]);
+//         setNewGoal('');
+//         setNewDueDate('');
+//       } catch (error) {
+//         console.error('Error adding goal:', error.message);
+//       }
+//     } else {
+//       console.error('Please enter a valid goal.');
+//     }
+//   };
+
+//   const handleEditGoal = (goalId) => {
+//     setEditingGoalId(goalId);
+//     const goalToEdit = goals.find((goal) => goal.id === goalId);
+//     setEditedGoalText(goalToEdit.content);
+//     setEditedDueDate(goalToEdit.dueDate);
+//   };
+
+//   const handleSaveEditedGoal = async () => {
+//     try {
+//       // Make a PUT request to update the edited goal
+//       const response = await fetch(URL + `goals/${editingGoalId}/`, {
+//         method: 'PUT',
+//         headers: {
+//           'Authorization': `Token ${localStorage.getItem('token')}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           content: editedGoalText,
+//           dueDate: editedDueDate,
+//           goal: "Updated Goal Description",
+//         }),
+//       });
+
+//       if (!response.ok) {
+//         throw new Error('Failed to save edited goal');
+//       }
+
+//       const updatedGoal = await response.json();
+//       const updatedGoals = goals.map((goal) =>
+//         goal.id === editingGoalId ? updatedGoal : goal
+//       );
+//       setGoals(updatedGoals);
+//       setEditingGoalId(null);
+//     } catch (error) {
+//       console.error('Error saving edited goal:', error.message);
+//     }
+//   };
+
+//   const handleDeleteGoal = async (goalId) => {
+//     try {
+//       // Make a DELETE request to remove the goal
+//       const response = await fetch(URL + `goals/${goalId}/`, {
+//         method: 'DELETE',
+//         headers: {
+//           'Authorization': `Token ${localStorage.getItem('token')}`,
+//         },
+//       });
+
+//       if (!response.ok) {
+//         throw new Error('Failed to delete goal');
+//       }
+
+//       const updatedGoals = goals.filter((goal) => goal.id !== goalId);
+//       setGoals(updatedGoals);
+//     } catch (error) {
+//       console.error('Error deleting goal:', error.message);
+//     }
+//   };
+
+//   useEffect(() => {
+//     // Fetch the existing goals when the component mounts
+//     const fetchGoals = async () => {
+//       try {
+//         const response = await fetch(URL + 'goals/', {
+//           headers: {
+//             'Authorization': `Token ${localStorage.getItem('token')}`,
+//           },
+//         });
+
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch goals');
+//         }
+
+//         const goalsData = await response.json();
+//         setGoals(goalsData);
+//       } catch (error) {
+//         console.error('Error fetching goals:', error.message);
+//       }
+//     };
+
+//     fetchGoals();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Create a New Goal</h2>
+//       <div>
+//         <label htmlFor="newGoal">Goal:</label>
+//         <input
+//           type="text"
+//           id="newGoal"
+//           value={newGoal}
+//           onChange={handleInputChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="newDueDate">Due Date:</label>
+//         <input
+//           type="date"
+//           id="newDueDate"
+//           value={newDueDate}
+//           onChange={handleDueDateChange}
+//         />
+//       </div>
+//       <button onClick={handleAddGoal}>Add Goal</button>
+
+//       <h2>Goal List</h2>
+//       {goals.length === 0 ? (
+//         <p>No Goals Added Yet</p>
+//       ) : (
+//         <ul>
+//           {goals.map((goal) => (
+//             <li key={goal.id}>
+//               {editingGoalId === goal.id ? (
+//                 <>
+//                   <input
+//                     type="text"
+//                     value={editedGoalText}
+//                     onChange={(e) => setEditedGoalText(e.target.value)}
+//                   />
+//                   <input
+//                     type="date"
+//                     value={editedDueDate}
+//                     onChange={(e) => setEditedDueDate(e.target.value)}
+//                   />
+//                   <button onClick={handleSaveEditedGoal}>Save</button>
+//                 </>
+//               ) : (
+//                 <>
+//                   {goal.content}
+//                   <button onClick={() => handleEditGoal(goal.id)}>Edit</button>
+//                   <button onClick={() => handleDeleteGoal(goal.id)}>Delete</button>
+//                 </>
+//               )}
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+
+//       <button>
+//         <Link to="/home" style={{ textDecoration: 'none', color: 'white' }}>
+//           Home
+//         </Link>
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Goal;
+
+
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 const URL = "http://localhost:3000/";
 
 const Goal = () => {
   const [newGoal, setNewGoal] = useState('');
-  const [newDueDate, setNewDueDate] = useState('');
+  const [newDueDate, setNewDueDate] = useState(null);
   const [goals, setGoals] = useState([]);
   const [editingGoalId, setEditingGoalId] = useState(null);
   const [editedGoalText, setEditedGoalText] = useState('');
-  const [editedDueDate, setEditedDueDate] = useState('');
+  const [editedDueDate, setEditedDueDate] = useState(null);
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setNewGoal(e.target.value);
   };
 
-  const handleDueDateChange = (e) => {
-    setNewDueDate(e.target.value);
+  const handleDueDateChange = (date) => {
+    setNewDueDate(date);
   };
 
   const handleAddGoal = async () => {
@@ -322,7 +528,7 @@ const Goal = () => {
         const newGoalItem = await response.json();
         setGoals([...goals, newGoalItem]);
         setNewGoal('');
-        setNewDueDate('');
+        setNewDueDate(null);
       } catch (error) {
         console.error('Error adding goal:', error.message);
       }
@@ -335,7 +541,7 @@ const Goal = () => {
     setEditingGoalId(goalId);
     const goalToEdit = goals.find((goal) => goal.id === goalId);
     setEditedGoalText(goalToEdit.content);
-    setEditedDueDate(goalToEdit.dueDate);
+    setEditedDueDate(goalToEdit.dueDate ? new Date(goalToEdit.dueDate) : null);
   };
 
   const handleSaveEditedGoal = async () => {
@@ -414,12 +620,59 @@ const Goal = () => {
     fetchGoals();
   }, []);
 
+  // Additional styles for horizontal navigation
+  const navStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#333', // Background color for the navigation bar
+    padding: '10px', // Add padding for better aesthetics
+    marginBottom: '80px', // Increase marginBottom to move the nav bar further up
+  };
+
+  const navItemStyle = {
+    margin: '0 15px', // Adjust spacing between navigation items
+  };
+
+  const navLinkStyle = {
+    color: 'white', // Text color for navigation links
+    textDecoration: 'none',
+    fontSize: '18px', // Adjust the font size of the links
+  };
+
+  // Adjustments for h1 and nav positions
+  const h1Style = {
+    fontFamily: 'Staatliches',
+    fontSize: '80px',
+    color: 'black',
+    marginTop: '20px', // Increase marginTop to move the h1 down
+  };
+
   return (
     <div>
+      <h1 style={h1Style}>Within</h1>
+
+      <nav style={navStyle}>
+        <ul style={{ listStyleType: 'none', padding: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <li style={navItemStyle}>
+            <Link to="/home" style={navLinkStyle}>Home</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/journal" style={navLinkStyle}>Journal Entries</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/profile" style={navLinkStyle}>Profile</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/logout" style={navLinkStyle}>Logout</Link>
+          </li>
+        </ul>
+      </nav>
+
       <h2>Create a New Goal</h2>
       <div>
-        <label htmlFor="newGoal">Goal:</label>
         <input
+          placeholder='Goal'
           type="text"
           id="newGoal"
           value={newGoal}
@@ -427,12 +680,11 @@ const Goal = () => {
         />
       </div>
       <div>
-        <label htmlFor="newDueDate">Due Date:</label>
-        <input
-          type="date"
-          id="newDueDate"
-          value={newDueDate}
+        <DatePicker
+          placeholderText="Select Due Date"
+          selected={newDueDate}
           onChange={handleDueDateChange}
+          dateFormat="MM/dd/yyyy"
         />
       </div>
       <button onClick={handleAddGoal}>Add Goal</button>
@@ -451,10 +703,11 @@ const Goal = () => {
                     value={editedGoalText}
                     onChange={(e) => setEditedGoalText(e.target.value)}
                   />
-                  <input
-                    type="date"
-                    value={editedDueDate}
-                    onChange={(e) => setEditedDueDate(e.target.value)}
+                  <DatePicker
+                    placeholderText="Select Due Date"
+                    selected={editedDueDate}
+                    onChange={(date) => setEditedDueDate(date)}
+                    dateFormat="MM/dd/yyyy"
                   />
                   <button onClick={handleSaveEditedGoal}>Save</button>
                 </>
--- src/pages/JournalNew.js
+++ src/pages/JournalNew.js
@@ -1,4 +1,3 @@
-// CSS - WITH DATEPICKER
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -12,23 +11,78 @@ const JournalNew = () => {
     setSelectedDate(date);
   };
 
+  // Additional styles for horizontal navigation
+  const navStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#333', // Background color for the navigation bar
+    padding: '10px', // Add padding for better aesthetics
+    marginBottom: '80px', // Increase marginBottom to move the nav bar further up
+  };
+
+  const navItemStyle = {
+    margin: '0 15px', // Adjust spacing between navigation items
+  };
+
+  const navLinkStyle = {
+    color: 'white', // Text color for navigation links
+    textDecoration: 'none',
+    fontSize: '18px', // Adjust the font size of the links
+  };
+
+  // Adjustments for h1 and nav positions
+  const h1Style = {
+    // textAlign: 'center',
+    fontSize: '80px',
+    color: 'black',
+    margin: '0',
+    marginTop: '20px', // Increase marginTop to move the h1 down
+    fontFamily: 'Staatliches',
+  };
+
+  const h2Style = {
+    textAlign: 'center',
+    fontSize: '60px',
+    color: 'black',
+    margin: '0',
+    fontFamily: 'Staatliches',
+    whiteSpace: 'nowrap',
+  };
+
   return (
     <div className="journal-new-container">
-      <h1>Create a Journal Entry</h1>
-      <h2>Reflect, Believe, Achieve</h2>
+      <h1 style={h1Style}>Within</h1>
+      <nav style={navStyle}>
+        <ul style={{ listStyleType: 'none', padding: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <li style={navItemStyle}>
+            <Link to="/home" style={navLinkStyle}>Home</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/goal" style={navLinkStyle}>Goals</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/profile" style={navLinkStyle}>Profile</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/logout" style={navLinkStyle}>Logout</Link>
+          </li>
+        </ul>
+      </nav>
+      <h2 style={h2Style}>Journal Your Way: Dream, Believe, Achieve</h2>
 
       <Form action="/createjournalentry" method="post">
         <div className="form-group">
-          <label>Title:</label>
+          {/* <label>Title:</label> */}
           <input type="text" name="title" placeholder="Journal Entry Title" />
         </div>
         <div className="form-group">
-          <label>Content:</label>
+          {/* <label>Content:</label> */}
           <textarea name="content" placeholder="Empower Your Thoughts Here..."></textarea>
         </div>
 
         <div className="form-group">
-          <label>Date:</label>
+          {/* <label>Date:</label> */}
           <DatePicker
             selected={selectedDate}
             onChange={handleDateChange}
