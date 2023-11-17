c9cb1ada00988e2a75c23aba7973f73f18b27eb7
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-17 16:53:47 -0500

    styling added to journal page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
 
-const AuthenticatedNavbar = () => {
-  const navigate = useNavigate();
+// const AuthenticatedNavbar = () => {
+//   const navigate = useNavigate();
 
-  const logout = () => {
-    localStorage.removeItem('token');
-    navigate('/login');
-  };
+//   const logout = () => {
+//     localStorage.removeItem('token');
+//     navigate('/login');
+//   };
 
-  return (
-    <div>
-      <nav>
-        <Link to="/goal">
-          <button>Goals</button>
-        </Link>
+//   return (
+//     <div>
+//       <nav>
+//         <Link to="/goal">
+//           <button>Goals</button>
+//         </Link>
 
-        <Link to="/journal">
-          <button>Journal Entries</button>
-        </Link>
+//         <Link to="/journal">
+//           <button>Journal Entries</button>
+//         </Link>
 
-        <Link to="/progress">
-          <button>Progress</button>
-        </Link>
+//         <Link to="/progress">
+//           <button>Progress</button>
+//         </Link>
 
-        <Link to="/profile">
-            <button>Profile</button>
-        </Link>
+//         <Link to="/profile">
+//             <button>Profile</button>
+//         </Link>
 
-        <button onClick={logout}>Log Out</button>
-      </nav>
-    </div>
-  );
-};
+//         <button onClick={logout}>Log Out</button>
+//       </nav>
+//     </div>
+//   );
+// };
 
-const Navbar = () => {
-  return localStorage.getItem('token') ? <AuthenticatedNavbar /> : null;
-};
+// const Navbar = () => {
+//   return localStorage.getItem('token') ? <AuthenticatedNavbar /> : null;
+// };
 
-export default Navbar;
+// export default Navbar;
