00084032cb0ff682390f308f01d72229f7b9c571
awoods12 <awoods12@terpmail.umd.edu>
2024-11-02 13:31:38 -0400

    Fixing up / route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div>
       <Outlet/>
-      <Welcome/>
     </div>
   );
 }
--- src/router.js
+++ src/router.js
@@ -17,12 +17,11 @@ const router = createBrowserRouter(
       <>
         <Route path="/" element={<App/>}>
             <Route path="/home" element={<Home/>}/>
-            <Route path='/Within' element={<Welcome/>}/>
+            <Route path='/' element={<Welcome/>}/>
+            <Route path='/within' element={<Welcome/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Signup/>}/>
-            {/* <Route path='/journal' element={<Journal/>}/> */}
             <Route path='/journal' element={<Journal2/>}/>
-            {/* <Route path="/goal" element={<Goal/>}/> */}
             <Route path="/goal" element={<Goal2/>}/>
             <Route path='/logout' element={<Logout/>}/>
         </Route>
